23633d39cc726c770c39df5abf1a971bde55bb17
lasconic <lasconic@users.noreply.github.com>
2022-04-25 01:00:04 +0000

    Update locale-specific data

--- wikidict/lang/es/langs.py
+++ wikidict/lang/es/langs.py
@@ -510,6 +510,7 @@ langs = {
     "kgk": "kaiwá",
     "kgm": "karipuna",
     "kha": "jasí",
+    "khb": "tai lü",
     "khi": "joisano",
     "khn": "jandeshi",
     "kho": "jotanés",
@@ -1188,5 +1189,5 @@ langs = {
     "zrp": "judeofrancés",
     "zu": "zulú",
     "zun": "zuñi",
-}  # 1,183
+}  # 1,184
 # END
