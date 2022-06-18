17515ac95fbf93a3cc8540edbc34089675b84cb5
lasconic <lasconic@users.noreply.github.com>
2022-06-18 01:10:09 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -50,8 +50,10 @@ regions = {
     "Bahia": "Bahia",
     "Baléares": "Baléares",
     "Bangladesh": "Bangladesh",
+    "Barrafranca": "Barrafranca",
     "Basses-terres continentales": "Basses-terres continentales",
     "Baugé": "Baugé",
+    "Bavière": "Bavière",
     "Beaujolais": "Beaujolais",
     "Belgique": "Belgique",
     "Belize": "Belize",
@@ -445,5 +447,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 440
+}  # 442
 # END
