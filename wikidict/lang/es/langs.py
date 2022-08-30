3def04e567579d17ace5f63ae3e94a5862b490f8
lasconic <lasconic@users.noreply.github.com>
2022-08-30 01:26:52 +0000

    Update locale-specific data

--- wikidict/lang/es/langs.py
+++ wikidict/lang/es/langs.py
@@ -577,6 +577,7 @@ langs = {
     "la": "latín",
     "la-lat": "latín tardío",
     "la-med": "latín medieval",
+    "la-neo": "neolatín",
     "la-vul": "latín vulgar",
     "lac": "lacandón",
     "lad": "judeoespañol",
@@ -1192,5 +1193,5 @@ langs = {
     "zu": "zulú",
     "zun": "zuñi",
     "zza": "zazaki",
-}  # 1,187
+}  # 1,188
 # END
