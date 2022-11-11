f05eb245785801d3710051d48a33e668761f826f
lasconic <lasconic@users.noreply.github.com>
2022-11-11 01:17:38 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -92,14 +92,12 @@ regions = {
     "Caucase": "Caucase",
     "Caux": "Pays de Caux",
     "Centrafrique": "Centrafrique",
+    "Centre du Viêt Nam": "Centre du Viêt Nam",
     "Champagne": "Champagne",
     "Champtocé": "Champtocé-sur-Loire",
-    "Charente-Maritime": "Charente-Maritime",
-    "Charentes": "Charentes",
     "Chef-Boutonne": "Chef-Boutonne",
     "Chenou": "Chenou",
     "Chili": "Chili",
-    "Chine": "Chine",
     "Cholet": "Cholet",
     "Chungcheong": "Chungcheong",
     "Château-Gontier": "Château-Gontier",
@@ -458,5 +456,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 453
+}  # 451
 # END
