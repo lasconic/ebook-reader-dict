7ccbcbcd8b96ca23a324d18d153b442e6bb98e4c
lasconic <lasconic@users.noreply.github.com>
2023-01-06 01:02:26 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -140,7 +140,6 @@ regions = {
     "Durango": "Durango",
     "Détroit": "Détroit",
     "ES": "Espagne",
-    "EU": "Europe",
     "Empire ottoman": "Empire ottoman",
     "Enna": "Enna",
     "Espagne": "Espagne",
@@ -456,5 +455,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 451
+}  # 450
 # END
