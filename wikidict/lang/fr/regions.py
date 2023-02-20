b5d08c6d8e63bafe48b8b4763deb2f07ca4a2ef3
lasconic <lasconic@users.noreply.github.com>
2023-02-20 01:06:58 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -231,6 +231,7 @@ regions = {
     "Latiano": "Latiano",
     "Le Fuilet": "Le Fuilet",
     "Le Havre": "Le Havre",
+    "Le Longeron": "Le Longeron",
     "Le Mans": "Le Mans",
     "Le Val-d’Ajol": "Le Val-d’Ajol",
     "Lecce": "Lecce",
@@ -505,5 +506,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 500
+}  # 501
 # END
