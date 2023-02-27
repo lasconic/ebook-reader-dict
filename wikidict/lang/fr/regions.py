6fc7674f8583ec3359be24bee08704c141d52403
lasconic <lasconic@users.noreply.github.com>
2023-02-27 01:04:45 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -411,7 +411,6 @@ regions = {
     "Saumur": "Saumur",
     "Sava": "Sava",
     "Savoie": "Savoie",
-    "Scorrano": "Scorrano",
     "Seclì": "Seclì",
     "Segré": "Segré",
     "Sercq": "Sercq",
@@ -504,7 +503,6 @@ regions = {
     "Équateur": "Équateur",
     "États-Unis": "États-Unis",
     "Île-de-France": "Île-de-France",
-    "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 501
+}  # 499
 # END
