88739f73fcd5e2a68eb2f12e89b8fe827edde3be
lasconic <lasconic@users.noreply.github.com>
2023-03-10 01:07:28 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -455,7 +455,6 @@ regions = {
     "Turquie": "Turquie",
     "Tyneside": "Tyneside",
     "UK": "Royaume-Uni",
-    "URSS": "URSS",
     "US": "États-Unis",
     "USA": "États-Unis",
     "Ukraine": "Ukraine",
@@ -507,5 +506,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 502
+}  # 501
 # END
