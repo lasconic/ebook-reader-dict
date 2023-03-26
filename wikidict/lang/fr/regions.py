0c15363d70154138c7dd6d8f1d23809bdf578f1c
lasconic <lasconic@users.noreply.github.com>
2023-03-26 01:05:30 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -199,6 +199,7 @@ regions = {
     "Haute-Marne": "Haute-Marne",
     "Hawaï": "Hawaï",
     "Haïti": "Haïti",
+    "Honduras": "Honduras",
     "Hongrie": "Hongrie",
     "IDLMadeleine": "Îles-de-la-Madeleine",
     "IE": "Irlande",
@@ -508,5 +509,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 503
+}  # 504
 # END
