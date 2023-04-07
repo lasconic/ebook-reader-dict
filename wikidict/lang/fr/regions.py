80baca3fb1e9d789e0074ced117f434f83654fd3
lasconic <lasconic@users.noreply.github.com>
2023-04-07 00:53:47 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -6,6 +6,7 @@ Auto-generated with `python -m scripts`.
 # START
 regions = {
     "AAE": "Anglais afro-américain",
+    "AD": "Andorre",
     "AE": "Émirats arabes unis",
     "AM": "Amérique latine",
     "AR": "Argentine",
@@ -108,8 +109,6 @@ regions = {
     "Champtocé": "Champtocé-sur-Loire",
     "Charente-Maritime": "Charente-Maritime",
     "Charentes": "Charentes",
-    "Chef-Boutonne": "Chef-Boutonne",
-    "Chenou": "Chenou",
     "Chili": "Chili",
     "Chine": "Chine",
     "Cholet": "Cholet",
@@ -160,7 +159,6 @@ regions = {
     "Europe": "Europe",
     "FR": "France",
     "FR-85": "Vendée",
-    "FR-Cher": "Cher",
     "Faeto": "Faeto",
     "Fontainebleau": "Fontainebleau",
     "Fougères": "Fougères",
@@ -509,5 +507,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 504
+}  # 502
 # END
