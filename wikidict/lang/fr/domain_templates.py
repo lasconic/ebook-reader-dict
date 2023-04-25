54b7f122051f107dcf4552c5ebff26528a90a724
lasconic <lasconic@users.noreply.github.com>
2023-04-25 00:59:45 +0000

    Update locale-specific data

--- wikidict/lang/fr/domain_templates.py
+++ wikidict/lang/fr/domain_templates.py
@@ -181,11 +181,9 @@ domain_templates = {
     "marsupial": "Zoologie",
     "marsupiaux": "Zoologie",
     "mers": "Géographie",
-    "meuble": "Mobilier",
     "meubles": "Mobilier",
     "meubles héraldiques": "Héraldique",
     "minéraux": "Minéralogie",
-    "mobilier": "Mobilier",
     "mollusques": "Malacologie",
     "monnaies": "Numismatique",
     "montagnes": "Géographie",
@@ -319,5 +317,5 @@ domain_templates = {
     "états": "État",
     "étoiles": "Astronomie",
     "îles": "Géographie",
-}  # 314
+}  # 312
 # END
