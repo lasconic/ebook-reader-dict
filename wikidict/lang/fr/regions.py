b01d6ebbcd3077cc324f0848ab0f436e3f6d8962
lasconic <lasconic@users.noreply.github.com>
2023-06-05 01:06:47 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -221,6 +221,7 @@ regions = {
     "Kazakhstan": "Kazakhstan",
     "Kerry": "Kerry",
     "Keveona": "Keveona",
+    "Kirghizistan": "Kirghizistan",
     "Kitigan Zibi": "Kitigan Zibi",
     "Kortemark": "Kortemark",
     "Koupuana": "Koupuana",
@@ -513,5 +514,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 508
+}  # 509
 # END
