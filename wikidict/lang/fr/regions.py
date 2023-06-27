7a2815e415dd4fd782e2a4efe35d788ba9e09699
lasconic <lasconic@users.noreply.github.com>
2023-06-27 01:13:52 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -65,6 +65,11 @@ regions = {
     "Bengale-Occidental": "Bengale-Occidental",
     "Berry": "Berry",
     "Bolivie": "Bolivie",
+    "Bordelais": "Bordelais",
+    "Boston": "Boston",
+    "Bourbonnais": "Bourbonnais",
+    "Bourgogne": "Bourgogne",
+    "Bray": "Pays de Bray",
     "Bretagne": "Bretagne",
     "Bretagne celtique": "Bretagne celtique",
     "Brindes": "Brindes",
@@ -89,7 +94,6 @@ regions = {
     "Canada": "Canada",
     "Canaries": "Canaries",
     "Caraïbes": "Caraïbes",
-    "Carovigno": "Carovigno",
     "Castri di Lecce": "Castri di Lecce",
     "Castrignano de’ Greci": "Castrignano de’ Greci",
     "Castro": "Castro",
@@ -509,5 +513,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 504
+}  # 508
 # END
