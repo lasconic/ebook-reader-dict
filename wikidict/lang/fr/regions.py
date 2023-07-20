df147f24f7c07e40ecc521955c1e9fcdaa174a85
lasconic <lasconic@users.noreply.github.com>
2023-07-20 01:04:41 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -94,10 +94,12 @@ regions = {
     "Canada": "Canada",
     "Canaries": "Canaries",
     "Caraïbes": "Caraïbes",
+    "Carovigno": "Carovigno",
     "Castri di Lecce": "Castri di Lecce",
     "Castrignano de’ Greci": "Castrignano de’ Greci",
     "Castro": "Castro",
     "Catalogne": "Catalogne",
+    "Catane": "Catane",
     "Caucase": "Caucase",
     "Caux": "Pays de Caux",
     "Cavallino": "Cavallino",
@@ -513,5 +515,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 508
+}  # 510
 # END
