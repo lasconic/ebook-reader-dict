d35dfbee870ee07a364380d7764f2b2d19110ccb
lasconic <lasconic@users.noreply.github.com>
2023-09-04 00:14:46 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -73,6 +73,7 @@ regions = {
     "Bhoutan": "Bhoutan",
     "Birmanie": "Birmanie",
     "Biélorussie": "Biélorussie",
+    "Bolivie": "Bolivie",
     "Bordelais": "Bordelais",
     "Bosnie-Herzégovine": "Bosnie-Herzégovine",
     "Boston": "Boston",
@@ -594,5 +595,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 589
+}  # 590
 # END
