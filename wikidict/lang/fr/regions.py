31b1964e4f8bac357c3a002d5d3f3fadb016135f
lasconic <lasconic@users.noreply.github.com>
2023-10-22 00:16:27 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -357,6 +357,7 @@ regions = {
     "Nevele": "Nevele",
     "New York": "New York",
     "Nicaragua": "Nicaragua",
+    "Nicosia": "Nicosia",
     "Niger": "Niger",
     "Nigeria": "Nigeria",
     "Nigéria": "Nigeria",
@@ -598,5 +599,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 593
+}  # 594
 # END
