91d4907364abc4fbb4e920926a18e785e5b083bd
lasconic <lasconic@users.noreply.github.com>
2023-11-30 00:15:24 +0000

    Update locale-specific data

--- wikidict/lang/pt/escopos.py
+++ wikidict/lang/pt/escopos.py
@@ -292,6 +292,7 @@ escopos = {
     "pi": "Piauí",
     "piauí": "Piauí",
     "planta": "botânica",
+    "plural": "plural",
     "popular": "popular",
     "porto": "Porto",
     "porto rico": "Porto Rico",
@@ -402,5 +403,5 @@ escopos = {
     "zootecnia": "veterinária",
     "áfrica do sul": "África do Sul",
     "índia": "{{#ifeq: {{{1}}}|pt|Índia|Índia}}",
-}  # 397
+}  # 398
 # END
