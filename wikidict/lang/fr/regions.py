10c37d840c18193f25525e6a031fc237674deac1
lasconic <lasconic@users.noreply.github.com>
2023-12-06 00:14:07 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -81,7 +81,6 @@ regions = {
     "Botswana": "Botswana",
     "Bourbonnais": "Bourbonnais",
     "Bourgogne": "Bourgogne",
-    "Bray": "Pays de Bray",
     "Bretagne": "Bretagne",
     "Bretagne celtique": "Bretagne celtique",
     "Brindes": "Brindes",
@@ -601,5 +600,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 596
+}  # 595
 # END
