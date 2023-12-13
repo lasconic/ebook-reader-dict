df348b2ad81d19f10e59f78e53b16fcb24f46a56
lasconic <lasconic@users.noreply.github.com>
2023-12-13 00:14:52 +0000

    Update locale-specific data

--- wikidict/lang/es/langs.py
+++ wikidict/lang/es/langs.py
@@ -1044,6 +1044,7 @@ langs = {
     "tig": "tigre",
     "tip": "trimuris",
     "tiv": "tiv",
+    "tji": "tujia septentrional",
     "tk": "turcomano",
     "tkl": "tokelauano",
     "tl": "tagalo",
@@ -1080,6 +1081,7 @@ langs = {
     "tsn": "tswana",
     "tt": "tártaro",
     "ttq": "tawallammat",
+    "ttt": "tati",
     "tum": "chitumbuka",
     "tup": "tupí",
     "tut": "altaico",
@@ -1225,5 +1227,5 @@ langs = {
     "zu": "zulú",
     "zun": "zuñi",
     "zza": "zazaki",
-}  # 1,220
+}  # 1,222
 # END
