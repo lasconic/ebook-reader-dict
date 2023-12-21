93283bbc98b90854cb6b010347c41a77cd075c22
lasconic <lasconic@users.noreply.github.com>
2023-12-21 00:14:38 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -192,6 +192,7 @@ regions = {
     "Franche-Comté": "Franche-Comté",
     "GB": "Royaume-Uni",
     "Gabon": "Gabon",
+    "Galatina": "Galatina",
     "Galatone": "Galatone",
     "Gallipoli": "Gallipoli",
     "Galway": "Galway",
@@ -600,5 +601,5 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 595
+}  # 596
 # END
