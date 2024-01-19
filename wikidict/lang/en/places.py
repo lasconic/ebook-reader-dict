3470a6564cf708ba597e78a6ddb758b95c9ec807
lasconic <lasconic@users.noreply.github.com>
2024-01-19 00:14:57 +0000

    Update locale-specific data

--- wikidict/lang/en/places.py
+++ wikidict/lang/en/places.py
@@ -2398,6 +2398,7 @@ recognized_placenames = {
     "country/Roman Empire": {"article": "the", "display": ""},
     "country/Solomon Islands": {"article": "the", "display": ""},
     "country/Soviet Union": {"article": "the", "display": ""},
+    "country/State of Palestine": {"article": "", "display": "country/Palestine"},
     "country/Swaziland": {"article": "", "display": "country/Eswatini"},
     "country/U.S.": {"article": "", "display": "country/United States"},
     "country/U.S.A.": {"article": "", "display": "country/United States"},
@@ -2498,5 +2499,5 @@ recognized_placenames = {
     "unincorporated territory/Northern Mariana Islands": {"article": "the", "display": ""},
     "unincorporated territory/United States Virgin Islands": {"article": "the", "display": ""},
     "valley/San Fernando Valley": {"article": "the", "display": ""},
-}  # 149
+}  # 150
 # END
